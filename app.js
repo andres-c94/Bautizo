0f0e83af81e3d2d85ed40157339694501d3bbe93
Andres C <69328357+andres-c94@users.noreply.github.com>
2022-05-24 22:48:39 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+document.getElementById("carta").style.display="none"
+
+function Tarjeta(){
+    document.getElementById("tarjet").style.display="none"
+    document.getElementById("img1").style.display="none"
+    document.getElementById("carta").style.display="block"
+    document.getElementById("titlee").style.display="none"
+    
+}
+
+function volver(){
+    document.getElementById("tarjet").style.display="block"
+    document.getElementById("img1").style.display="block"
+    document.getElementById("carta").style.display="none"
+    document.getElementById("titlee").style.display="block"
+}
+
